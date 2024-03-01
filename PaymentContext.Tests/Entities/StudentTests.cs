a7d803679d8908e81fb1467524b5874ac665e7bb
Jaqueline Manzano <jaque_manzanos@hotmail.com>
2024-02-29 21:43:03 -0300

    Aula  Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var student = new Student ("Jaque", "Manzano", "123456789", "jaque@hotmail.com");
+
+        var subscription = new Subscription(null);
+        student.AddSubscription(subscription);
+    }
+}
